b6650dc3f6ca0db0995bf807eb127ca1b5308214
YGBWI <12088396+aghsfhsgvdgh@user.noreply.gitee.com>
2024-05-12 12:50:15 +0800

    222

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("master");
-        System.out.println(111);
+        System.out.println(222);
     }
 }
